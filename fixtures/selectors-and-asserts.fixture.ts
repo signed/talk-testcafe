75b2bfce54f234982bf5cf02e01ed769636a7159
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-07-14 10:48:52 +0200

    re structure the layout a bit

--- fixtures/selectors-and-asserts.fixture.ts
+++ fixtures/selectors-and-asserts.fixture.ts
@@ -4,7 +4,19 @@ fixture('asserts and ')
   .page('http://localhost:8080/');
 
 test('record calls', async (tc) => {
-  await tc.click(Selector('[data-automation-id=change-with-random-delay__reveal]'))
-    .expect(Selector('[data-automation-id=change-with-random-delay__surprise]').innerText)
+  const revealButton = Selector('[data-automation-id=change-with-random-delay__reveal]');
+  const surpriseText = Selector('[data-automation-id=change-with-random-delay__surprise]').innerText;
+
+  await tc.click(revealButton)
+    .expect(surpriseText)
     .eql('revealed');
+});
+
+fixture('testcafe example').page('http://devexpress.github.io/testcafe/example');
+
+test.meta('', '')('test name', async tc => {
+  const sliderValues = Selector('.slider-value');
+  await tc
+    .expect(sliderValues.count).eql(10)
+    .hover(sliderValues.withText('8'))
 });
---
+++ fixtures/walkthrough.ts
@@ -0,0 +1,37 @@
+/*
+ - Basic tests structure
+ - Test hook before/after
+ - Selectors
+ - Assertions
+ */
+import 'test-code-structure.fixture';
+import 'hello-world-fixture';
+import 'test-hooks.fixture';
+import 'selectors-and-asserts.fixture';
+
+/*
+ - Client Functions
+ - Request Mocks
+ - Live Mode
+ */
+
+/*
+ - Configuration CLI/Configuration file/API
+ - Run/Debug from intellij
+ */
+
+/*
+ - command line runner
+ - configuration file
+ - write your own runner with the offical api
+ */
+
+/*
+ # Quirks
+ - Console Error Abort
+ - debug break points lost between runs
+ - browser reload after debugging too long
+ - hot reload in live mode
+ - Not all Promises are equal
+ */
+
