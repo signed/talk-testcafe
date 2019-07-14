757ff2c840da6265c10f4002fd797d2159f77d7c
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-07-14 22:47:26 +0200

    make the example page more descriptive

--- src/local-storage-display.tsx
+++ src/local-storage-display.tsx
@@ -12,14 +12,21 @@ export default class LocalStorageDisplay extends React.Component<Props, State> {
 
   constructor(props: Props) {
     super(props);
-    this.state = { value: '' };
+    this.state = {value: ''};
   }
 
   public render(): React.ReactNode {
+    const keyValue = this.state.value === '' ? (<span>nothing there</span>) :
+      (<React.Fragment>
+        {this.props.localStorageKey}: <span data-automation-id="value-from-local-storage">{this.state.value}</span>
+      </React.Fragment>);
+
     return (
       <div>
-        <button data-automation-id="load-from-local-storage" type="button" onClick={() => this.loadValue()}>load</button>
-        Value: <span data-automation-id="value-from-local-storage">{this.state.value}</span>
+        <button data-automation-id="load-from-local-storage" type="button" onClick={() => this.loadValue()}>
+          load value from local storage
+        </button>
+        {keyValue}
       </div>
     );
   }
@@ -27,7 +34,7 @@ export default class LocalStorageDisplay extends React.Component<Props, State> {
   private loadValue() {
     const value = window.localStorage.getItem(this.props.localStorageKey);
     if (value) {
-      this.setState({ value });
+      this.setState({value});
     }
   }
 }
