64441f126c31d466ac27236921ed393439cab2d8
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-07-14 20:51:01 +0200

    test to showcase promise problem

--- fixtures/gotchas.fixture.ts
+++ fixtures/gotchas.fixture.ts
@@ -1,5 +1,5 @@
 import 'testcafe';
-import {ClientFunction} from 'testcafe';
+import {ClientFunction, Selector} from 'testcafe';
 
 fixture('testcafe gotchas')
   .page('http://localhost:8080/');
@@ -16,15 +16,18 @@ test('watch what you log', async (tc) => {
   await tc.expect(logs.log).contains('hello in the client');
 });
 
-test('not all promises are equal', async (tc) =>{
+const emailInput = Selector('#email-input');
+
+test.only('not all promises are equal', async (tc) => {
+  await tc.typeText(emailInput, 'initial@example.org');
+  await tc.expect(emailInput.value).eql('initial@example.org');
+});
+
+test('restarts browser if you are stuck in a client code breakpoint for too long', async (tc) => {
 
 });
 
-test('restarts browser if you are stuck in a client code breakpoint for too long', async (tc) =>{
-
-});
-
-test('data passed between client (application under test) and server (testcafe) is serialized and de-serialized', async (tc) =>{
+test('data passed between client (application under test) and server (testcafe) is serialized and de-serialized', async (tc) => {
 
 });
 
