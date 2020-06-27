18557d7705a381fdd88cab33dc84ee617a8bfea3
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2020-06-27 21:36:07 +0200

    print all log messages

--- fixtures/console-log.fixture.ts
+++ fixtures/console-log.fixture.ts
@@ -3,6 +3,7 @@ fixture('stack trace from application on console')
 
 test('read log output from the browser', async (tc) => {
     const messages = await tc.getBrowserConsoleMessages();
+    messages.log.forEach(message => console.log(message));
 
     await tc.expect(messages.log.length).eql(1)
         .expect(messages.log[0]).eql('hello testcafe');
